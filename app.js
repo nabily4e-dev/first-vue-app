309a1f4f4f30333eeda3775330c2472b209c1bd6
nabily4e-dev <nbl.dv21@gmail.com>
2021-05-02 23:54:15 +0300

    Adding project files

--- app.js
+++ app.js
@@ -9,14 +9,19 @@ const app = Vue.createApp({
         }
     },
     methods: {
-        getUser() {
+        async getUser() {
             // console.log(this.firstName);
 
-            this.firstName = 'Sam',
-            this.lastName='Smith',
-            this.email='sam@gmail.com',
-            this.gender='female',
-            this.picture= 'https://randomuser.me/api/portraits/women/10.jpg'
+            const res = await fetch('https://randomuser.me/api')
+            const { results } = await res.json()
+
+            console.log(results)
+
+            this.firstName = results[0].name.first
+            this.lastName=results[0].name.last
+            this.email=results[0].email
+            this.gender=results[0].gender
+            this.picture= results[0].picture.large
 
         }
     },
